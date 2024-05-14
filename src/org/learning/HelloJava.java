d9b3c0ac22cac1ad1d2a7589e87fafd36cbe189e
maicolkutchoukov <maicol.j.95@gmail.com>
2024-05-14 13:02:36 +0200

    add println

--- src/org/learning/HelloJava.java
+++ src/org/learning/HelloJava.java
@@ -3,5 +3,6 @@ package org.learning;
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        System.out.println("And GoodVibes");
     }
 }
